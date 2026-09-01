7949569d1d11438729ab14fb870189f317d2e419
zhangrunzhao <zhangrunzhao1@gmail.com>
2026-09-02 02:12:53 +0800

    feat: 实现移动逻辑

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,5 +3,9 @@ extends Node2D
 var direction: Vector2 = Vector2(1,0)
 var speed := 5
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
 	position += direction * speed
+	
+	if Input.is_action_just_pressed("confirm"):
+		print("something")
