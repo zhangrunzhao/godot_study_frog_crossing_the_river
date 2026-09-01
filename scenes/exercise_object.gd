7a67b32901b81d5a52b8ca14302fda8d2dca296d
zhangrunzhao <zhangrunzhao1@gmail.com>
2026-09-02 02:35:32 +0800

    feat: 添加碰撞

--- scenes/exercise_object.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _physics_process(_delta: float) -> void:
-	scale = scale * 1.001
--- scenes/player.gd
+++ scenes/player.gd
@@ -1,11 +1,12 @@
-extends Node2D
+extends CharacterBody2D
 
 var direction: Vector2 = Vector2(1,0)
-var speed := 5
+var speed := 200
 
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed
+	velocity = direction * speed
+	move_and_slide()
 	
 	if Input.is_action_just_pressed("confirm"):
 		print("something")
