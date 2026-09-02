1ef8bf6b9e62be6b13ebae76cbc4ad42a6d0badd
zhangrunzhao <zhangrunzhao@bytedance.com>
2026-09-02 21:35:31 +0800

    feat: 添加动画跟信号

---
+++ scenes/game.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+func _on_finish_area_2d_body_entered(body: Node2D) -> void:
+	print(body)
+	print("已进入")
+	pass # Replace with function body.
--- scenes/player.gd
+++ scenes/player.gd
@@ -6,7 +6,18 @@ var speed := 200
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed
+	animation()
 	move_and_slide()
 	
 	if Input.is_action_just_pressed("confirm"):
 		print("something")
+
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = 'left'
+		else:
+			$AnimatedSprite2D.animation = 'up' if direction.y < 0 else 'down'
+	else:
+		$AnimatedSprite2D.frame = 0
